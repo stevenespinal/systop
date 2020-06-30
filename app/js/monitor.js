afd850f56532b957d46d5ec8aa1d3fa2b26eb564
Steven <stevenjesusespinal@gmail.com>
2020-06-30 01:10:26 -0400

    added dynamic progress bar

---
+++ app/js/monitor.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const osu = require("node-os-utils");
+const cpu = osu.cpu;
+const mem = osu.mem;
+const os = osu.os;
+
+let cpuOverload = 10;
+
+setInterval(async () => {
+    const info = await cpu.usage();
+    document.getElementById('cpu-usage').innerText = `${info.toFixed(2)}%`;
+    document.getElementById('cpu-progress').style.width = info + '%';
+
+    if (info >=cpuOverload) {
+        document.getElementById('cpu-progress').style.background = 'red';
+    } else {
+        document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
+
+    const free = await cpu.free();
+    document.getElementById('cpu-free').innerText = `${free.toFixed(2)}%`;
+    document.getElementById('system-uptime').innerText = secondsToDhms(os.uptime());
+}, 1000);
+
+document.getElementById('cpu-model').innerText = cpu.model();
+document.getElementById('comp-name').innerText = os.hostname();
+document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
+
+(async function () {
+    const info = await mem.info();
+    console.log(info);
+    document.getElementById('memory-total').innerText = info.totalMemMb;
+})();
+
+function secondsToDhms(sec) {
+    sec = +sec;
+    const d = Math.floor(sec / (3600 * 24));
+    const h = Math.floor((sec % (3600 * 24)) / 3600);
+    const m = Math.floor((sec % 3600) / 60);
+    const s = Math.floor(sec % 60);
+
+    return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
